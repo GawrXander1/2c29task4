3021cb693f358b07fe0e8f382f181a2ed66bcfc6
GawrXander1 <115450405+GawrXander1@users.noreply.github.com>
2022-10-24 14:58:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,36 @@
-let count = 0
 let yva = 0
-input.onButtonPressed(Button.A, function () {
-    count = 0
-    while (count <= 4) {
+let count = 0
+let ab = 0
+input.onButtonPressed(Button.B, function () {
+    yva = 0
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
         led.plot(count, 0)
         basic.pause(500)
-        basic.clearScreen()
         count += 1
     }
 })
-input.onButtonPressed(Button.B, function () {
-    yva = 0
-    while (yva <= 4) {
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
         led.plot(0, yva)
         basic.pause(500)
-        basic.clearScreen()
         yva += 1
+    }
+})
+basic.forever(function () {
+    ab = 0
+    if (input.buttonIsPressed(Button.AB)) {
+        while (ab <= 4) {
+            basic.clearScreen()
+            led.plot(ab, 0)
+            basic.pause(500)
+            ab += 1
+        }
+        while (ab >= 4) {
+            ab += -1
+        }
     }
 })
